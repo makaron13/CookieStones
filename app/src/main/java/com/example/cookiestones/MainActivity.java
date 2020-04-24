56904d58aad053ffc7b47035f3d91542dcfa89ae
makaron13 <alex.shm13@gmail.com>
2020-04-24 14:04:09 +0300

    Initial commit

--- app/src/main/java/com/example/cookiestones/MainActivity.java
+++ app/src/main/java/com/example/cookiestones/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvMovies;
